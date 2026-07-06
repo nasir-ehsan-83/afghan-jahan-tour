2ecf47528524108aae2d45507a981a5d7231fbef
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-07-06 16:46:59 +0430

    feat(validation): add 'createUserSchema' validation in users/users.validator.ts

---
+++ src/modules/users/users.validator.ts
@@ -0,0 +1,9 @@
+import z from "zod";
+
+export const createUserSchema = z.object({
+    name: z.string().optional(),
+    username: z.string().min(3, "Username length should not be less than 3 characters"),
+    email: z.string().email("Email Format is not Valid").min(7, "Email's length should not be less than 7 characters"),
+    password: z.string().min(8, "Password's length should not be less than 8 characters"),
+    role: z.enum(["ADMIN", "USER"]),
+});
