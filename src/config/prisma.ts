7dd5f07d1702cc0377bf8d17b4d01b3b373c55f0
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-07-07 00:00:13 +0430

    update 'prisma.ts' using 'PrismaPg' and 'Pool'

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,17 +1,28 @@
-import { PrismaClient }  from "../generated/prisma/index.js";
+import { PrismaClient } from "../generated/prisma/index.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
 
 const prismaClientSingleton = () => {
-    return new PrismaClient({
-        log: ["query", "info", "warn", "error"]
-    });
+  return new PrismaClient({
+    adapter,
+    log: process.env.NODE_ENV === "development" 
+      ? ["query", "info", "warn", "error"] 
+      : ["error"],
+  });
 };
 
 declare global {
-    var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
 export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== "production") {
-    globalThis.prismaGlobal = prisma;
+  globalThis.prismaGlobal = prisma;
 }
