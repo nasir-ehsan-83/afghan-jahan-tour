2da2b13fd30903c85dd604c6abf47067f2d778f1
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-07-07 15:09:36 +0430

    feat(error-handling): implement not-found error-handling in errors/not-found.handler.ts

---
+++ src/common/errors/index.ts
@@ -0,0 +1,4 @@
+export * from "./app.error.js";
+export * from "./http.error.js";
+export * from "./error.handler.js";
+export * from "./not-found.handler.js";
---
+++ src/common/errors/not-found.handler.ts
@@ -0,0 +1,8 @@
+import type { Request, Response } from "express";
+
+export const notFoundHandler = (req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: `Route ${req.method} ${req.path} not found`,
+  });
+};
